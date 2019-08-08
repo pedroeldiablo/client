381bd0b14ebdc35d8c2ccbd507212a136403cdc4
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-08 13:12:10 +0100

    wires up react router and scaffolds signup form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={createStore(reducers, {})}>
+        <BrowserRouter>
+            <App>
+                <Route path="/" exact component={Welcome} />
+                <Route path="/signup" component={Signup} />
+            </App>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('#root')
+);
